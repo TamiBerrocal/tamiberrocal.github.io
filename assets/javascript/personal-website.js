0f68edaec7bcef19888fde43d84ec0ce1e88c25d
Tamara Berrocal <tamaraberrocal@gmail.com>
2019-08-29 03:56:10 -0300

    Versión simple terminada

--- assets/javascript/personal-website.js
+++ assets/javascript/personal-website.js
@@ -55,21 +55,30 @@ window.onload = () => {
     const typed = document.querySelector('.typed');
     const aboutMe = document.querySelectorAll('.about-me');
     const contact = document.querySelectorAll('.contact');
-    const socialNetworks = document.querySelectorAll('.social-network');
+    const contactContainer = document.querySelectorAll('.contact-container');
+    const socialNetwork = document.querySelectorAll('.social-network');
+    const goodbye = document.querySelectorAll('.goodbye');
+    const language = document.querySelector('.language');
+    const es = document.querySelectorAll('.es');
+    const en = document.querySelectorAll('.en');
 
     let colors = [...COLORS];
     let coloredNodes = [];
 
     aboutMe.forEach(element => coloredNodes.push({ node: element, color: getRandomElement(colors) }));
     contact.forEach(element => coloredNodes.push({ node: element, color: getRandomElement(colors) }));
-    socialNetworks.forEach(element => coloredNodes.push({ node: element, color: getRandomElement(colors) }));
-
-    console.log(coloredNodes);
+    contactContainer.forEach(element => coloredNodes.push({ node: element, color: getRandomElement(colors) }));
+    socialNetwork.forEach(element => coloredNodes.push({ node: element, color: getRandomElement(colors) }));
+    goodbye.forEach(element => coloredNodes.push({ node: element, color: getRandomElement(colors) }));
 
     coloredNodes.filter(el => el.node.classList.contains("social-network")).forEach(el => el.node.style.color = el.color);
-
     coloredNodes.forEach(el => el.node.addEventListener("mouseover", function() { this.style.backgroundColor = el.color; }));
     coloredNodes.forEach(el => el.node.addEventListener("mouseout", function() { this.style.backgroundColor = "white"; }));
+
+    language.addEventListener("click", () => {
+        es.forEach(el => el.classList.toggle("hidden"));
+        en.forEach(el => el.classList.toggle("hidden"));
+    });
 
     let texts = typed.getAttribute('data-texts');
     let period = typed.getAttribute('data-period');
