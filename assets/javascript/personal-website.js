aa98fb263b510c3f9f36426afc3c2df07ea2e535
Tamara Berrocal <tamaraberrocal@gmail.com>
2019-09-18 03:02:25 -0300

    Eventos funcionando bien

--- assets/javascript/personal-website.js
+++ assets/javascript/personal-website.js
@@ -1,13 +1,9 @@
 const COLORS = ["#ff9999", "#feff57", "#7af996"];
 const BACKGROUND_COLORS = ["#f0c9d7", "#ffdfba", "#ffffba", "#baffc9", "#ffddd1", "#c0f0f4"];
 
-const addStyle = (element, style) => {
-    element.classList.add(style);
-}
+const addStyle = (element, style) => element.classList.add(style);
 
-const removeStyle = (element, style) => {
-    element.classList.remove(style);
-}
+const removeStyle = (element, style) => element.classList.remove(style);
 
 const changeStyles = (element, added, removed) => {
     addStyle(element, added);
@@ -60,105 +56,86 @@ const replaceText = (element) => {
     setTimeout(() => updateText(element, element.innerHTML, TYPE), WAIT);
 }
 
-const getRandomElement = (array) => {
+const getRandomElement = array => {
     return array.splice([Math.floor(Math.random() * array.length)], 1)[0];
 }
 
 const setBackgroundColor = color => {
-    return element => element.style.backgroundColor = color;
+    return element => () => {
+        element.style.backgroundColor = color;
+    }
 }
 
 const setBorder = (style, color) => {
-    return element => {
+    return element => () => {
         element.style.borderStyle = style;
         element.style.borderColor = color;
     }
 }
 
 const setColor = color => {
-    return element => element.style.color = color;
-}
-
-
-const addStyleToElements = (elements, style) => {
-    [...elements].map(el => style(el));
-}
-
-const addEventToElements = (elements, event, callback) => {
-    [...elements].map(el => el.addEventListener(event, () => callback(el)));
-}
-
-/*
-const addColor = (element, color) => {
-    return () => {
-        element.setAttribute("data-color", color);
-        element.style.backgroundColor = color;
+    return element => () => {
+        element.style.color = color;
     }
 }
 
-const addBorder = (element, style, color) => {
-    return () => {
-        element.style.borderStyle = style;
-        element.style.borderColor = color;
-    }
+const addRandomBackgroundColor = (elements, colors) => {
+        [...elements].map(element => {
+        element.setAttribute("data-background-color", getRandomElement(colors));
+        setBackgroundColor(element.getAttribute('data-background-color'))(element)();
+        setBorder("solid", element.getAttribute('data-background-color'))(element)();
+    });
 }
-*/
+
+const addRandomColor = (elements, colors) => [...elements].map(element => setColor(getRandomElement(colors))(element)());
 
 const addRandomBackgroundColorOnHover = (elements, colors) => {
-    addEventToElements(elements, "mouseover", setBackgroundColor());
-    //addEventToElements(elements, "mouseover", setBorder("solid", getRandomElement(colors)));
-    addEventToElements(elements, "mouseout", setBackgroundColor("inherit"));
-
-    /*
-    [...elements].map(el => {
-        let color = getRandomElement(colors);
-        el.addEventListener("mouseover", addColor(el, color));
-        el.addEventListener("mouseover", addBorder(el, "solid", color));
-        el.addEventListener("mouseout", addColor(el, "inherit"));
-        el.addEventListener("mouseover", addBorder(el, "solid", "inherit"));
+    [...elements].map(element => {
+        element.setAttribute("data-background-color", getRandomElement(colors));
+        element.addEventListener("mouseover", setBackgroundColor(element.getAttribute('data-background-color'))(element));
+        element.addEventListener("mouseover", setBorder("solid", element.getAttribute('data-background-color'))(element));
+        element.addEventListener("mouseout", setBackgroundColor("transparent")(element));
+        element.addEventListener("mouseout", setBorder("solid", "transparent")(element));
     });
-*/
 }
 
-/*
-const removeBackgroundColorOnHover = elements => {
-    [...elements].map(el => {
-        el.addEventListener("mouseover", function() {
-            el.parentElement.parentElement.removeEventListener("mouseover", addColor);
-            el.parentElement.parentElement.addEventListener("mouseover", addColor(el.parentElement.parentElement, "transparent"));
-            el.parentElement.parentElement.addEventListener("mouseover", addBorder(el.parentElement.parentElement, "dashed", el.style.color));
+const removeGrandparentBackgroundColorOnHover = elements => {
+    [...elements].map(element => {
+        let grandparent = element.parentElement.parentElement;
+
+        element.addEventListener("mouseover", function() {
+            grandparent.removeEventListener("mouseover", setBackgroundColor);
+            grandparent.removeEventListener("mouseover", setBorder);
+            grandparent.addEventListener("mouseover", setBackgroundColor("transparent")(grandparent));
+            grandparent.addEventListener("mouseover", setBorder("dashed", element.style.color)(grandparent));
         });
-        el.addEventListener("mouseout", function() {
-            el.parentElement.parentElement.removeEventListener("mouseover", addColor);
-            el.parentElement.parentElement.removeEventListener("mouseover", addBorder);
-            el.parentElement.parentElement.addEventListener("mouseover", addColor(el.parentElement.parentElement, "yellow"));
-            addBorder(el, "solid", "white");
+        element.addEventListener("mouseout", function() {
+            grandparent.removeEventListener("mouseover", setBackgroundColor);
+            grandparent.removeEventListener("mouseover", setBorder);
+            grandparent.addEventListener("mouseover", setBackgroundColor(grandparent.getAttribute('data-background-color'))(grandparent));
+            grandparent.addEventListener("mouseover", setBorder("solid", grandparent.getAttribute('data-background-color'))(grandparent));
         });
     });
 }
-*/
 
 window.onload = () => {
     const greeting = document.querySelectorAll(".greeting");
     const textES = document.querySelectorAll(".text.es, .es > .text");
     const textEN = document.querySelectorAll(".text.en, .en > .text");
     const section = document.querySelectorAll(".bio, .contact, .goodbye");
-    const socialNetwork = document.querySelectorAll(".social-networks > a");
+    const socialNetwork = document.querySelectorAll(".social-network");
     const language = document.querySelector(".language");
     const es = document.querySelectorAll(".es");
     const en = document.querySelectorAll(".en");
 
     let typed = document.querySelector(".typed");
 
-//    addRandomBackgroundColor(textEN, [...COLORS]);
-//    addRandomBackgroundColor(textES, [...COLORS]);
-//    addRandomColor(socialNetwork, [...COLORS]);
-    addStyleToElements(textEN, setBackgroundColor(getRandomElement([...COLORS])));
-    addStyleToElements(textES, setBackgroundColor(getRandomElement([...COLORS])));
-    addStyleToElements(socialNetwork, setColor(getRandomElement([...COLORS])));
+    addRandomBackgroundColor(textEN, [...COLORS]);
+    addRandomBackgroundColor(textES, [...COLORS]);
+    addRandomColor(socialNetwork, [...COLORS]);
 
     addRandomBackgroundColorOnHover(section, [...BACKGROUND_COLORS]);
-//    removeBackgroundColorOnHover(socialNetwork);
+    removeGrandparentBackgroundColorOnHover(socialNetwork);
 
     language.addEventListener("click", () => {
         [...es].map(el => el.classList.toggle("hidden"));
